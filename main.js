5c27f678dcbb80b70f8498c98051fd6078417c3c
Julio Cesar <172700125+julioldv@users.noreply.github.com>
2026-05-14 19:07:39 -0600

    Add initial library logic

---
+++ main.js
@@ -0,0 +1,44 @@
+const myLibrary = [];
+
+
+function Book(title, author, totalPages, isRead,id){
+    if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.totalPages = totalPages;
+    this.isRead = isRead;
+    this.id = id;
+
+    this.info = function (){
+        const readStatus = this.isRead ? "already read" : "not read yet";
+        
+
+
+        return `${this.title} by ${this.author}, ${this.totalPages} pages, ${readStatus}`;
+    }
+
+}
+
+function addBookToLibrary(title, author, totalPages, isRead){
+    let id = crypto.randomUUID();
+    let book = new Book (title, author, totalPages, isRead, id);
+    myLibrary.push(book);
+}
+
+
+addBookToLibrary("Atomic Habits", "James Clear",200,false);
+addBookToLibrary("The Hobbit", "Tolkien",400,true);
+addBookToLibrary("Pedro Paramo", "Juan Rulfo",200,false);
+
+function showLibrary(){
+    myLibrary.forEach(book => {
+        console.log(book.info());
+    });
+}
+
+
+showLibrary();
+
+
